0a4d90833b00c4a6a7776db47a342ccda0e31f3b
Brianna Forster <briannaforster@gmail.com>
2024-01-06 16:55:38 -0800

    Add app main page text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <header>
+        <h1>Archivist</h1>
+        <h2>
+          A Dungeons & Dragons 5th Edition<br />
+          Character Creation and Leveling Companion App
+        </h2>
+      </header>
+      <nav>
+        <a>+ Create a Character</a>
+      </nav>
     </div>
   );
 }
