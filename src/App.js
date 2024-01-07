5c1de95363201784c4181e7a43bb52e500c7c366
Brianna Forster <briannaforster@gmail.com>
2024-01-07 00:18:56 -0800

    Use Material UI for modal and form components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-import Modal from 'react-modal';
 import { Header, ModalContent } from './components';
 
-Modal.setAppElement(document.getElementById('root'));
 
 function App() {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-  // function afterOpenModal() {
-  // references are now sync'd and can be accessed.
-  // }
-  function closeModal() {
-    setIsOpen(false);
-  }
-
   return (
     <div className="App">
       <Header />
-      <nav>
-        <a onClick={openModal}>+ Create a Character</a>
-      </nav>
-      <Modal
-        isOpen={modalIsOpen}
-        // onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        contentLabel="Create Character"
-      >
-        <ModalContent />
-      </Modal>
+      <ModalContent />
     </div>
   );
 }
--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -1,25 +1,75 @@
+import * as React from 'react';
 import './ModalContent.css';
+import {
+    Box, Button, Dialog, DialogActions, DialogContent, DialogContentText,
+    DialogTitle, Divider, FormControl, Input, InputLabel, MenuItem,
+    Select, Slider, TextField
+} from '@mui/material';
+
 
 function ModalContent() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div class="modal-content">
-            <header><h1>{'Roll a New Character'}</h1></header>
-            <form>
-                <label for="race-dropdown">Race:</label>
-                <select id="race-dropdown" name="race">
-                    <option value="human">Human</option>
-                    <option value="orc">Orc</option>
-                </select>
-                <br />
-                <label for="new-char-str">Strength: </label>
-                <input type="integer" id="new-char-str" class="new-char-stat handwritten" size="3" name="stats[strength]"></input>
-                <span class="stat-bonus" id="strength"></span>
-                <button type="button" class="die-img" disabled>20</button>
-                <input type="range" min="1" max="20"></input>
-                <br />
-                <button type="submit">Save & Close</button>
-            </form>
-        </div>
+        <React.Fragment>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                <a>+ Create a Character</a>
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Roll a New Character</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Enter your character's description and attributes below.
+                    </DialogContentText>
+                    <Box
+                        component="form"
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <InputLabel id="race-dropdown-label">Race:</InputLabel>
+                        <Select
+                            labelId="race-dropdown-label"
+                            id="race-dropdown"
+                            label="Race"
+                            fullWidth
+                        >
+                            <MenuItem value={'human'}>Human</MenuItem>
+                            <MenuItem value={'orc'}>Orc</MenuItem>
+                        </Select>
+                        <Divider />
+                        <InputLabel id="str-input-label">Strength: </InputLabel>
+                        <Input
+                            type="number" id="str-input" labelId="str-input-label" name="stats[strength]"
+                            sx={{ width: '3em' }}
+                        />
+                        &nbsp;
+                        {/* <span class="stat-bonus" id="strength"></span>
+                        <button type="button" class="die-img" disabled>20</button> */}
+                        <Slider
+                            aria-label="Strength"
+                            defaultValue={0}
+                            // getAriaValueText={valuetext}
+                            valueLabelDisplay="auto"
+                            min={1}
+                            max={20}
+                            sx={{ width: '10em', verticalAlign: 'middle', marginLeft: '3em' }}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Save & Close</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
     );
 }
 
