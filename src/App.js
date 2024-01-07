96e418c1ec4f25286395b49dc055957bb11929ed
Brianna Forster <briannaforster@gmail.com>
2024-01-06 17:53:34 -0800

    Import react-modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
 import './App.css';
+import Modal from 'react-modal';
 import Header from './Header';
 
+Modal.setAppElement(document.getElementById('root'));
+
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  // function afterOpenModal() {
+  // references are now sync'd and can be accessed.
+  // }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="App">
       <Header />
       <nav>
-        <a>+ Create a Character</a>
+        <a onClick={openModal}>+ Create a Character</a>
       </nav>
+      <Modal
+        isOpen={modalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Create Character"
+      ></Modal>
     </div>
   );
 }
