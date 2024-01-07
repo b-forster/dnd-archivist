82c7b8554c61b480fc554e54c3b648775a9df3a7
Brianna Forster <briannaforster@gmail.com>
2024-01-06 17:03:27 -0800

    Extract Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Archivist</h1>
-        <h2>
-          A Dungeons & Dragons 5th Edition<br />
-          Character Creation and Leveling Companion App
-        </h2>
-      </header>
+      <Header />
       <nav>
         <a>+ Create a Character</a>
       </nav>
