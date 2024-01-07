c96fef45ee269d246a052c55e68fc7f9250ffb48
Brianna Forster <briannaforster@gmail.com>
2024-01-07 00:18:56 -0800

    Use Material UI for modal and form components

--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import './ModalContent.css';
 import {
@@ -83,30 +82,6 @@ function ModalContent() {
                 </DialogActions>
             </Dialog>
         </React.Fragment>
-=======
-import './ModalContent.css';
-
-function ModalContent() {
-    return (
-        <div class="modal-content">
-            <header><h1>{'Roll a New Character'}</h1></header>
-            <form>
-                <label for="race-dropdown">Race:</label>
-                <select id="race-dropdown" name="race">
-                    <option value="human">Human</option>
-                    <option value="orc">Orc</option>
-                </select>
-                <br />
-                <label for="new-char-str">Strength: </label>
-                <input type="integer" id="new-char-str" class="new-char-stat handwritten" size="3" name="stats[strength]"></input>
-                <span class="stat-bonus" id="strength"></span>
-                <button type="button" class="die-img" disabled>20</button>
-                <input type="range" min="1" max="20"></input>
-                <br />
-                <button type="submit">Save & Close</button>
-            </form>
-        </div>
->>>>>>> 6286bce (Add ModalContent component with some basic create character form elements)
     );
 }
 
