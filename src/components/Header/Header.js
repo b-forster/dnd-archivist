7877c27343384416b644a6e05fc89fa8fedc20f9
Brianna Forster <briannaforster@gmail.com>
2024-01-06 20:39:25 -0800

    Add header fonts and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-// import './Header.css';
+import './Header.css';
 
 function Header() {
     return (
         <header>
             <h1>Archivist</h1>
             <h2>
-                A Dungeons & Dragons 5th Edition<br />
-                Character Creation and Leveling Companion App
+                A Character Leveling Companion App<br />
+                for Dungeons & Dragons 5th Edition
             </h2>
         </header>
     );
