1476829c1802a1de28d585a20836d51a295e4531
Brianna Forster <briannaforster@gmail.com>
2024-03-29 17:20:22 -0700

    Cleaning up main branch

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import { default as Header } from './Header/Header';
-import { default as ModalContent } from './ModalContent/ModalContent';
-import { default as AbilityRow } from './AbilityRow/AbilityRow';
-
-export { Header, ModalContent, AbilityRow };
